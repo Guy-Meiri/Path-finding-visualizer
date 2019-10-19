421cdba6e6160f0b34852728f268a40477cce8c3
Guy-Meiri <guymeiri312@gmail.com>
2019-10-20 02:17:04 +0300

    Added Buttons and a the start of the UI script

---
+++ PathFindingVisualizer/Assets/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    private General m_GameScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_GameScript = GetComponent<General>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnAStarButtonPressed()
+    {
+        Debug.Log("a star pressed");
+    }
+}
