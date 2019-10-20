a04dffbfb74b4a19d1c4e60247a5c79afdb91e6d
Guy-Meiri <guymeiri312@gmail.com>
2019-10-20 22:46:00 +0300

    added a boardSize slider to the UI and UI manager

--- PathFindingVisualizer/Assets/uIManager.cs
+++ PathFindingVisualizer/Assets/uIManager.cs
@@ -7,6 +7,7 @@ public class uIManager : MonoBehaviour
     [SerializeField]
     private GameObject m_GraphObject;
     private General m_GameScript;
+    private int m_BoardSize = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -47,5 +48,16 @@ public class uIManager : MonoBehaviour
     public void OnVolumeChanged(float volume)
     {
         m_GameScript.DrawSpeed = -volume;
+    }
+
+    public void OnBoardSizeChanged(float i_newSize)
+    {
+        int newBoardSize = (int)i_newSize;
+        if(newBoardSize != m_BoardSize)
+        {
+            m_BoardSize = newBoardSize;
+            Debug.Log("size" + (int)i_newSize);
+        }
+        
     }
 }
